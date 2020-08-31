f0fbec14a524110e1bf3e098578f0cb3f98edfd6
MarkShizzle <45970975+markshizzle@users.noreply.github.com>
2020-08-31 21:26:50 +0200

    Added counter

--- content.js
+++ content.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
     var currentPage = "";
     var pages;
+    var count = 0;
     chrome.runtime.onMessage.addListener(
         function(request) {
-            $('#scrape-info').remove();
+            initInfo();
             if(request.message === "start" ) {
                 pages = getPages('.font-semibold.mb-1.text-primary span');
-                initInfo();
                 var i = 0;
                 $.each(pages, function(key){
                     setTimeout(function(){
+                        count += 1;
                         getNextPage('span:contains('+key+')');
+                        incrementCount();
                         currentPage = key;
-                        initInfo();
-                        updatePages(pages);
                     },i);
                     i += 12500;
                 });
-
             }
             if(request.message === "getCurrentPageData") {
                 pages[currentPage] = request.data;
@@ -34,8 +33,9 @@ $(document).ready(function() {
 
     function initInfo() {
         var sHtml = '<div class="scrape-info" id="scrape-info">';
-        sHtml += '<h1>Scrape-info</h1>';
-        sHtml += '<div id="scrape-data"></div></div>';
+        sHtml += '<h1>Progress:</h1><br>';
+        sHtml += '<p id="count">'+count+'</p>';
+        sHtml += '</div>';
         $("body").append(sHtml);
     }
 
@@ -47,17 +47,10 @@ $(document).ready(function() {
         return pages;
     }
 
-    function updatePages(data) {
-        $('#scrape-data').empty();
-        var sHtml = '';
-        $.each(data, function(name, values) {
-            sHtml += '<div id="page"><p>'+name+'</p><br>';
-            $.each(values, function(i, value) {
-                sHtml += '<p>'+value+'</p><br>'
-            })
-            sHtml += '</div>';
-        });
-        $("#scrape-data").append(sHtml);
+    function incrementCount() {
+        var newCount = parseInt($('#count').text()) + 1;
+        $('#count').remove();
+        $("#scrape-info").append('<p id="count">'+newCount+'</p>');
     }
 
 });
